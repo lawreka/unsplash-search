8dc5f216e67a327257eceb0ca93dc9b72310916a
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2023-10-29 13:31:10 +0100

    add favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: "Unsplash search app",
+    description: `Kathryn's awesome coding test for Zora.co`,
+}
 
 export default function RootLayout({
     children,
@@ -7,6 +13,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            </head>
             <body>{children}</body>
         </html>
     )
